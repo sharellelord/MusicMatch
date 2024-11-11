490fc26504ba2630ce90b76abba25b2ab4c57400
bas99323 <bas99323@uga.edu>
2024-11-11 15:54:14 -0500

    Added routing from login to recs page

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import React from 'react';
+import { useRouter } from 'next/navigation'; 
 import styles from './Login.module.css';
 
 
 const Login = () => {
+
+  const router = useRouter();
+
+  // Handle form submission
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Form validation/signup logic goes here
+
+    // Goes to reccomendation page
+    router.push('/add-recommendation');
+  };
+
   return (
     <div className={`card ${styles.loginCard}`}>
       <img src="https://png.pngtree.com/png-vector/20240903/ourmid/pngtree-rainbow-headphones-music-clipart-illustration-png-image_13743867.png" alt="Headphones" className={styles.headphonesIcon} />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit} >
         <input className={styles.formInput} type="text" placeholder="Username" />
         <input className={styles.formInput} type="password" placeholder="Password" />
         <button type="submit" className={`button ${styles.loginButton}`}>Log In</button>
--- src/app/components/SignUp.tsx
+++ src/app/components/SignUp.tsx
@@ -1,19 +1,34 @@
+"use client";
+
 import React from 'react';
+import { useRouter } from 'next/navigation'; 
 import styles from './SignUp.module.css';
 
-
 const SignUp = () => {
+  const router = useRouter();
+
+  // Handle form submission
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Form validation/signup logic goes here
+
+    // Goes to reccomendation page
+    router.push('/add-recommendation');
+  };
+
   return (
     <div className={`card ${styles.signupCard}`}>
-      <form className={styles.form}>
-        <input className={styles.formInput} type="email" placeholder="Email" />
-        <input className={styles.formInput} type="text" placeholder="Username" />
-        <input className={styles.formInput} type="password" placeholder="Password" />
-        <input className={styles.formInput} type="password" placeholder="Confirm Password" />
-        <button type="submit" className={`button ${styles.signupButton}`}>Sign Up</button>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input className={styles.formInput} type="email" placeholder="Email" required />
+        <input className={styles.formInput} type="text" placeholder="Username" required />
+        <input className={styles.formInput} type="password" placeholder="Password" required />
+        <input className={styles.formInput} type="password" placeholder="Confirm Password" required />
+        <button type="submit" className={`button ${styles.signupButton}`}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default SignUp;
