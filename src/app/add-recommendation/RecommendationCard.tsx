09410bc76397f1057c25838ca551b384c221b316
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-11-11 23:36:09 -0500

    Update RecommendationCard.tsx

--- src/app/add-recommendation/RecommendationCard.tsx
+++ src/app/add-recommendation/RecommendationCard.tsx
@@ -2,17 +2,28 @@
 
 import React from 'react';
 import styles from './RecommendationCard.module.css';
+import Image from 'next/image';
+
 
 interface RecommendationCardProps {
+  imageUrl: string;
   artist: string;
   genre: string;
   vibes: string;
   popularity: string;
 }
 
-const RecommendationCard: React.FC<RecommendationCardProps> = ({ artist, genre, vibes, popularity }) => {
+const RecommendationCard: React.FC<RecommendationCardProps> = ({ imageUrl, artist, genre, vibes, popularity }) => {
   return (
     <div className={styles.card}>
+      <Image
+        className={styles.userImg}
+        src={imageUrl}
+        alt={`${artist} avatar`}
+        width={100}
+        height={100}
+        priority
+      />
       <h3>{artist}</h3>
       <p><strong>Genre:</strong> {genre}</p>
       <p><strong>Vibes:</strong> {vibes}</p>
