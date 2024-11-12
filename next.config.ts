816a8e91c97eefba6e37f84d4401b75a7cfde2b9
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-11-11 23:36:59 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {  images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'i1.sndcdn.com',
+      port: '',
+      pathname: '/avatars-yHA8nds2mqg4uYtr-kyxTzw-t1080x1080.jpg',
+    },
+    {
+      protocol: 'https',
+      hostname: 'www.hollywoodinsider.com',
+      port: '',
+      pathname: '/wp-content/uploads/2020/09/Hollywood-Insider-Frank-Ocean-Letter-From-Fan.jpg',
+    },
+    {
+      protocol: 'https',
+      hostname: 'media.allure.com',
+      port: '',
+      pathname: '/photos/66ff11de6c3f3d8fc0e06ded/**', // Allow all paths under this pattern
+    },
+  ],
+},
 };
-
 export default nextConfig;
