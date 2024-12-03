0d0da3ba20ad7394af8f03e0bbf5fb4cc6beaa4b
sharellelord <151474451+sharellelord@users.noreply.github.com>
2024-12-02 23:36:25 -0500

    added comments

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -6,32 +6,37 @@ import { signIn } from "next-auth/react";
 import styles from "./Login.module.css";
 
 const Login = () => {
-  const router = useRouter();
+  const router = useRouter(); // initialize router for nav
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
 
+  // handles form submission
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    // validate input fields
     if (!username || !password) {
-      setError('All fields are required.');
+      setError('All fields are required.'); //ensures all fields filled
       return;
     }
 
     try {
+      // sign in with credentials provider
       const res = await signIn("credentials", {
         username,
         password,
         redirect: false,
       });
 
+      // handle errors in auth
       console.log(res);
       if (res?.error) {
         setError("Wrong username or password");
         return;
       } 
 
+      // redirect to add-recommendation page on successful login
       router.push("/add-recommendation");
     } catch (error) {
       console.log(error);
@@ -45,16 +50,16 @@ const Login = () => {
         alt="Headphones"
         className={styles.headphonesIcon}
       />
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={handleSubmit}> {/* Form for login input */}
         <input
           className={styles.formInput}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}  // Update username state on change
           type="text"
           placeholder="Username"
         />
         <input
           className={styles.formInput}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}  // Update password state on change
           type="password"
           placeholder="Password"
         />
